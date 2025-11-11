2ca905326193ac485bd395a2da7349ab074b11e8
Laboni Biswas <140432394+labonibiswas@users.noreply.github.com>
2025-11-11 20:55:00 +0530

    Time: 1 ms (66.47%), Space: 47.3 MB (8.31%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,43 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        boolean firstRow = false; boolean firstCol = false;
+
+        //set markers in the 1st row and first col
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                if(matrix[i][j] == 0){
+                    if(i == 0){
+                        firstRow = true;
+                    } 
+                    if(j == 0){
+                        firstCol = true;
+                    } 
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+
+        //replace inner matrix
+        for(int i=1; i<matrix.length; i++){
+            for(int j=1; j<matrix[0].length; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        //last remaining checks
+        if(firstRow){
+            for(int j=0; j<matrix[0].length; j++){
+                matrix[0][j] = 0;
+            }
+        }
+        if(firstCol){
+            for(int i=0; i<matrix.length; i++){
+                matrix[i][0] = 0;
+            }
+        }
+
+    }
+}
