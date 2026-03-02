862c1e0da51ee85230414376711d2987be8b1351
Laboni Biswas <140432394+labonibiswas@users.noreply.github.com>
2026-03-02 23:29:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,33 +1,20 @@
 class Solution {
     public int countPrimes(int n) {
-        //1. base case
-        if(n <=2){
-            return 0;
-        }
-        //2. array initialize and set all element as 1(true).
-        boolean isPrime[] = new boolean[n];
-        for(int i=2; i<n; i++){
-            isPrime[i] = true;
-        }
+        boolean[] isComposite = new boolean[n];
 
-        //3. sieve operation
-        //check prime upto sqrt(i)
-        for(int i=2; i*i<n; i++){
-            if(isPrime[i]){
-                for(int j=i*i; j<n; j += i){
-                    isPrime[j] = false;
+        if(n <2) return 0;
+
+        int count =0;
+        for(int i=2; i<n; i++){
+            if(!isComposite[i]){
+                count++;
+                if((long) i*i < n){
+                    for(int j=i*i; j<n; j+=i){
+                        isComposite[j] = true;
+                    }
                 }
             }
         }
-
-        //4. count prime
-        int count=0;
-        for(int i=2; i<n; i++){
-            if(isPrime[i]){
-                count++;
-            }
-        }
-
         return count;
     }
 }
