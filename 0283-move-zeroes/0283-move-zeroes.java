86c722bc86eae8eae0e5232c218d8fc4b44fde99
Laboni Biswas <140432394+labonibiswas@users.noreply.github.com>
2026-03-03 01:04:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,9 +1,10 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int l=0;
-        for(int r=0; r<nums.length; r++){
+        int n = nums.length;
+        int l=0; 
+        for(int r=0; r<n; r++){
             if(nums[r] != 0){
-                if(l != r){
+                if( r != l){
                     int temp = nums[r];
                     nums[r] = nums[l];
                     nums[l] = temp;
@@ -11,6 +12,5 @@ class Solution {
                 l++;
             }
         }
-
     }
 }
