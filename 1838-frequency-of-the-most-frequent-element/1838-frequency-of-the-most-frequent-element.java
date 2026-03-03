4a7472623c31d3b36960cc6c525e1a43310a3f42
Laboni Biswas <140432394+labonibiswas@users.noreply.github.com>
2026-03-03 23:21:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
@@ -1,26 +1,20 @@
 class Solution {
     public int maxFrequency(int[] nums, int k) {
         Arrays.sort(nums);
-        int l=0, res=0;
-        long total=0;
-        int n= nums.length;
-        
-        for(int r=0; r<n; r++){
+        int l = 0, res = 0;
+        long total = 0;
+
+        for (int r = 0; r < nums.length; r++) {
             total += nums[r];
-            long windowLen = (r-l+1);
-            long sum = windowLen * nums[r];
-            long cost = sum - total;
-            while(cost > k){
-                total -= nums[l];
-                l++;
-
-                windowLen = (long)(r - l + 1);
-                sum = windowLen * nums[r];
-                cost = sum - total;
+            
+            // While the cost to make all elements in window equal to nums[r] > k
+            // Cost = (Number of elements * Target value) - Current Sum
+            while ((long)nums[r] * (r - l + 1) - total > k) {
+                total -= nums[l++];
             }
-            res = Math.max(res, r-l+1);
+            
+            res = Math.max(res, r - l + 1);
         }
-
         return res;
     }
 }
