6f555b60423efaeaff3665505fb2074dfa26df51
Laboni Biswas <140432394+labonibiswas@users.noreply.github.com>
2026-03-04 17:26:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,19 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        int temp = 0, rev = 0;
+        int rev = 0; 
         int copy = x;
-        if( x < 0 || ( x % 10 == 0 && x != 0)){
-            return false;
-        }
-        while( copy != 0){
-            temp = copy % 10;
-            rev = rev * 10 + temp;
-            copy = copy / 10;
-        }
-        if(rev == x){
-            return true;
+        if(x <0) return false;
+
+        while(copy != 0){
+            int rem = copy%10;
+            rev = rev*10 + rem;
+            copy /= 10;
         }
 
-        return false;
+        return rev == x;
     }
 }
