d71393f611b285face97768263f0bb913c704163
Laboni Biswas <140432394+labonibiswas@users.noreply.github.com>
2026-03-12 08:04:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -10,7 +10,6 @@ class Solution {
             if(flowerbed[i] == 0){
                 if(isLeftEmplty && isRightEmpty){
                     count++;
-                    flowerbed[i] = 1;
                 }
             }
         }
