c679182e8b5519dfb28fb372e518cce510c7a3b7
Laboni Biswas <140432394+labonibiswas@users.noreply.github.com>
2026-03-22 20:37:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -3,9 +3,10 @@ class Solution {
         int idx = 0;
         for(int i=0; i<chars.length; i++){
             char ch = chars[i];
-            int count =0; 
-            while(i<chars.length && ch == chars[i]){
-                count++;i++;
+            int count = 0;
+            while(i <chars.length && ch == chars[i]){
+                count++; 
+                i++;
             }
             if(count == 1) chars[idx++] = ch;
             else{
