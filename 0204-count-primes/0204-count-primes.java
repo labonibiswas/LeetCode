581a824a28360eed420d9c8987b460b3601a1770
Laboni Biswas <140432394+labonibiswas@users.noreply.github.com>
2026-03-26 07:30:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,16 +1,14 @@
 class Solution {
     public int countPrimes(int n) {
-        boolean[] isComposite = new boolean[n];
-
-        if(n <2) return 0;
-
-        int count =0;
+        boolean[] arr = new boolean[n];
+        if(n<2) return 0;
+        int count=0;
         for(int i=2; i<n; i++){
-            if(!isComposite[i]){
+            if(!arr[i]){
                 count++;
-                if((long) i*i < n){
+                if((long) i*i<n){
                     for(int j=i*i; j<n; j+=i){
-                        isComposite[j] = true;
+                        arr[j] = true;
                     }
                 }
             }
