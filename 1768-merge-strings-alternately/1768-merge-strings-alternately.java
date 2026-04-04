533d4bf93cfadb3c0a085feee7b0ec7d4a6cb2e5
Laboni Biswas <140432394+labonibiswas@users.noreply.github.com>
2026-04-04 13:05:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,14 +1,21 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-       
+        if(word1.length() == 0) return word2;
+        if(word2.length() == 0) return word1;
+        int n1 = word1.length();
+        int n2 = word2.length();
         StringBuilder str = new StringBuilder();
-        int i = 0;
-        while(i < word1.length() || i < word2.length()){
-            if(i < word1.length()) str.append(word1.charAt(i));
-            if(i < word2.length()) str.append(word2.charAt(i));
-            i++;
+        int l = Math.max(word1.length(), word2.length());
+        for(int i=0; i<l; i++){
+            if(n1 >0){
+                str.append(word1.charAt(i));
+                n1--;
+            } 
+            if(n2 >0){
+                str.append(word2.charAt(i));
+                n2--;
+            } 
         }
-
         return str.toString();
     }
 }
