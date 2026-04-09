7397d2770cf22244a2a44a3dd8a0e365828373a3
Laboni Biswas <140432394+labonibiswas@users.noreply.github.com>
2026-04-09 12:12:57 +0530

    Time: 0 ms (100%), Space: 43.6 MB (93.08%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -5,10 +5,13 @@ class Solution {
             int mid = l + (r-l) / 2;
             if(nums[mid] == target) return mid;
 
+            //sorted
             if(nums[l] <= nums[mid]){
-                if(target >= nums[l] && target < nums[mid]) r = mid-1;
-                else l = mid+1;
-            }else{
+                if(target >= nums[l] && target < nums[mid]) r = mid-1; // if target is in sorted part
+                else l = mid+1; // target is in unsorted part
+            }
+            //unsorted
+            else{
                 if(target > nums[mid] && target <= nums[r]) l = mid + 1;
                 else r = mid-1;
             }
