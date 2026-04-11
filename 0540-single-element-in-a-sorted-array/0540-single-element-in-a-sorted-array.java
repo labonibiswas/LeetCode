34b3dc010908604fcf866583e0d7d846c4a9aad0
Laboni Biswas <140432394+labonibiswas@users.noreply.github.com>
2026-04-11 13:31:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -4,10 +4,10 @@ class Solution {
         int ans = nums[0];
         for(int i=0; i<nums.length; i++){
             if(i == 0){
-                if(nums[i] != nums[i+1]) return nums[i];
+                if(nums[i] != nums[i+1]) return nums[i]; // we have nest the if condition else the pointer will not increase
             } 
             else if(i == nums.length-1) {
-                if(nums[i] != nums[i-1]) return nums[i];
+                if(nums[i] != nums[i-1]) return nums[i]; // we have nest the if condition else the pointer will not increase
             }
             else {
                 if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) return nums[i];
