c97ce4045a955a9927e97c905132314e89ee0f34
Laboni Biswas <140432394+labonibiswas@users.noreply.github.com>
2026-04-13 11:37:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,24 +1,23 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        int l=0, r=nums.length-1;
+        int min = Integer.MAX_VALUE;
+        int l=0, r= nums.length-1;
         while(l<=r){
-            int mid = l + (r-l)/2;
-            if(nums[mid] == target) return true;
-            //when mid, left element and right element becomes equal we can't compare whether the target is in sorted or unsorted part hence we continue to shrink both left and right pointers
-            if(nums[mid] == nums[l] && nums[mid] == nums[r]){
+            int mid = l + (r-l) /2;
+            if(target == nums[mid]) return true;
+            if(nums[l] == nums[mid] && nums[r] == nums[mid]){
                 l++;
                 r--;
                 continue;
             }
             if(nums[l] <= nums[mid]){
-                if(target >= nums[l] && target < nums[mid]) r = mid-1;
+                if(target >= nums[l] && target < nums[mid]) r = mid - 1;
                 else l = mid+1;
             }else{
-                if(target > nums[mid] && target <= nums[r]) l = mid+1;
+                if(target > nums[mid] && target <= nums[r]) l = mid + 1;
                 else r = mid - 1;
             }
         }
-
         return false;
     }
 }
