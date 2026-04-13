c6fe26f0573af9ddc1f106490293165780c334d0
Laboni Biswas <140432394+labonibiswas@users.noreply.github.com>
2026-04-13 09:20:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -9,8 +9,8 @@ class Solution {
                 if(target >= nums[l] && target < nums[mid]) r = mid-1;
                 else l = mid+1;
             }else{
-                if(target <= nums[r] && target < nums[mid]) l = mid-1;
-                else r = mid+1;
+                if(target <= nums[r] && target > nums[mid]) l = mid+1;
+                else r = mid-1;
             }
         }
         return -1;
